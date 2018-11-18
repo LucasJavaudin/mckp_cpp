e26c5598aeb7d305d5eed25c82f30e01e61bb86d
flerest <44815742+flerest@users.noreply.github.com>
2018-11-19 00:26:52 +0100

    Update mckp.cpp

--- mckp.cpp
+++ mckp.cpp
@@ -1,5 +1,6 @@
 #include <random>
 #include <chrono>
+#include <iostream>
 
 #include "data.h"
 
@@ -15,5 +16,14 @@ int main() {
 	lognormal_distribution<double> weight_distribution(0.0, 1.0);
 
 	// Create dataset.
-	Dataset data(1000, 10, value_distribution, weight_distribution, generator);
+	Dataset data(100, 100, value_distribution, weight_distribution, generator);
+
+	// Test de la fonction upperBound() qui rend l'enveloppe convexe d'une classe
+	Class c(vector<double>({1,3,2.3,4,1,0.9,1.9,1.5,4.5}),vector<double>({1,2,2.5,3,3.5,4,4.2,4.5,4.7}));
+    Class c2 = c.upperBound();
+    c2.affiche();
+	pair<double,double> a = MCKP_Greedy_Algorithm(data,100);
+	cout << "poids vide " << a.first << " valeur totale " << a.second;
+	// data.getClasses()[0]->getItems()[0]->affiche();
+	return 0;
 }
